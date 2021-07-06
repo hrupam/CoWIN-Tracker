670ff4c3313ce65d39d80e45d6ec7f86b7ddfc92
Rupam Hari <hrupam55@gmail.com>
2021-07-06 13:54:19 +0530

    changed description and added creator name

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <div className="header">
+        <span style={{ fontSize: "18px" }}>Made with ❤️ by Rupam Hari</span>
         <span className="title">CoWIN Tracker</span>
         <span className="date">
           Current Date: <span style={{ fontWeight: "bold" }}>{date}</span>{" "}
